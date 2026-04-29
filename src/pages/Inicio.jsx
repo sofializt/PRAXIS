5567eeabfe58a36696b20856848f35879b69d6d7
Sofia Lizcano <sofializcanot@gmail.com>
2026-04-29 15:06:49 -0500

    correccis

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -214,9 +214,9 @@ export default function Inicio({ onSoyProfesor, onJuzga }) {
           </span>
         </div>
 
-      {(!usuario || usuario.id_rol === 4) && (
         <button
-          onClick={cerrarSesion}
+          onClick={handleSoyProfesor}
+          disabled={animandoProfesor}
           style={{
             backgroundColor: "white",
             color: "#00482B",
@@ -233,7 +233,6 @@ export default function Inicio({ onSoyProfesor, onJuzga }) {
         >
           SOY PROFESOR
         </button>
-      )}
       </nav>
 
       {/* CONTENIDO */}
